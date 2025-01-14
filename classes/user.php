b74dadc2a6a16a0ed62b4760a7a2d82944cb9a9a
khadiijaa123 <Khadijasahnoun70@gmail.com>
2025-01-14 17:11:53 +0100

    ajouter la classe user

---
+++ classes/user.php
@@ -0,0 +1,75 @@
+<?php 
+
+require_once '../database/db.php';
+
+class User{
+    
+    private $id_user;
+    private $nom;
+    private $prenom;
+    private $email;
+    private $profile;
+    private $Motdepasse;
+    private PDO $role;
+
+    private $db;
+
+public function __construct($db)
+{
+    $this->db= $db;
+}
+
+public function getIduser(){
+    return $this->id_user;
+}
+
+public function getNom(){
+    return $this->nom;
+}
+public function getPrenom(){
+    return $this->prenom;
+}
+public function getEmail(){
+    return $this->email;
+}
+ public function getProfile(){
+   return  $this->profile;
+ }
+
+ public function getRole(){
+    return $this->role;
+ }
+
+ public function setIduser($id_user){
+     $this->id_user= $id_user;
+ }
+
+ public function setNom($nom){
+     $this->nom =$nom;
+ }
+ public function setPrenom($prenom){
+    $this->prenom =$prenom;
+ }
+ public function setEmail($email){
+    $this->email= $email;
+ }
+ public function setProfile($profile){
+    $this->profile = $profile;
+ }
+ public function setRole($role){
+    $this->role= $role;
+ }
+
+ public function getData(){
+    $id_user = $_SESSION['id_user'];
+    $query ="SELECT * FROM user WHERE id_user = :id_user";
+    $stmt= $this->db->prepare($query);
+    $stmt->execute([':id_user'=> $id_user]);
+    return $stmt->fetch(PDO::FETCH_ASSOC);
+ }
+  
+
+}
+
+
+?>
