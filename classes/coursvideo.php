a0cb3a6618581fb1e6abf26c12aa3fc38ca34943
khadiijaa123 <Khadijasahnoun70@gmail.com>
2025-01-15 12:52:59 +0100

    ajouter la fonction de afficher cours

--- classes/coursvideo.php
+++ classes/coursvideo.php
@@ -1,7 +1,44 @@
 <?php
 require_once '../classes/cours.php';
 class Coursvideo extends Cours{
+         
+    private $db;
+     public function __construct($db)
+     {
+        $this->db= $db ;
+     }
+
     public function afficherCours(){
-        
+    try{
+            $sql="SELECT 
+            Cours.Titre,
+            Cours.DESCRIPTION,
+            Cours.video,
+            Cours.pdf,
+            Cours.Statut,
+            Cours.DateCréation,
+            Cours.DateModification,
+            Category.Nom AS NomCategorie,
+            tags.Nom AS NomTag
+            FROM 
+                Cours
+            INNER JOIN 
+                Category ON Cours.id_category = Category.id_category
+            INNER JOIN 
+                tags ON Cours.id_tag = tags.id_tag;
+        ";
+        $stmt= $this->db->prepare($sql);
+        $stmt->execute();
+        if($stmt->rowcount()>0){
+            $result= $stmt->fetchAll();
+            return $result;
+        }else{
+            return "Aucun cours trouvé" ;
+        }
+
+    }catch(PDOException $e){
+            echo "Erreur lors de la récupération des cours : " .$e->getMessage();
+        }
     }
 }
+
--- views/ajoutercours.php
+++ views/ajoutercours.php
@@ -37,6 +37,8 @@ if($_SERVER['REQUEST_METHOD']=== 'POST' && isset($_POST['Titre'])){
 
 
 
+
+
 ?>
 
 
